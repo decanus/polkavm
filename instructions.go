3ab680a2ea76d015dd5b9613c087d7724fb7e19c
decanus <7621705+decanus@users.noreply.github.com>
2024-11-23 22:31:47 +0700

    rm

--- instructions.go
+++ instructions.go
@@ -7,18 +7,6 @@ type RegisterIndexes struct {
 // InstructionFunc represents a PVM instruction handler
 type InstructionFunc = func(ctx Context, ri RegisterIndexes, skip uint32) error
 
-func ra(data []byte) uint8 {
-	return uint8(min(uint64(data[0])&0x1111, 12)) // @TODO double check
-}
-
-func rb(data []byte) uint8 {
-	return min(data[0]>>4, 12)
-}
-
-func rd(data []byte) uint8 {
-	return min(data[1], 12)
-}
-
 func add(ctx Context, ri RegisterIndexes, skip uint32) error {
 	ra := ctx.State.Registers[ri.A]
 	rb := ctx.State.Registers[ri.B]
