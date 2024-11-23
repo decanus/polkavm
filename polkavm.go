6289a00d2174b0ddb92e036dff9f691286528580
decanus <7621705+decanus@users.noreply.github.com>
2024-11-23 22:22:48 +0700

    impl

--- polkavm.go
+++ polkavm.go
@@ -8,9 +8,23 @@ type Context struct {
 type Program struct {
 }
 
+func (p *Program) SkipAt(idx uint32) uint32 {
+
+}
+
 // InstructionAt returns the instruction at idx
 func (p *Program) InstructionAt(idx uint32) (OpCode, error) {
 	return 0, nil // @TODO
+}
+
+// RegisterIndexesAt returns the register for instruction at  indexes at idx
+func (p *Program) RegisterIndexesAt(idx uint32) RegisterIndexes {
+
+}
+
+// GasCostAt returns the gas cost for instruction at index at idx
+func (p *Program) GasCostAt(idx uint32) uint64 {
+
 }
 
 type Registers [13]uint32
@@ -28,25 +42,6 @@ func Execute() {
 
 }
 
-// let pc = context.state.pc
-//
-//	let skip = program.skip(pc)
-//	let inst = program.getInstructionAt(pc: pc)
-//
-//	guard let inst else {
-//	    return .exit(.panic(.invalidInstructionIndex))
-//	}
-//
-//	// TODO: check after GP specified the behavior
-//	if context.state.program.basicBlockIndices.contains(pc) {
-//	    let blockGas = context.state.program.getBlockGasCosts(pc: pc)
-//	    context.state.consumeGas(blockGas)
-//	    logger.debug("consumed \(blockGas) gas for block at pc: \(pc)")
-//	}
-//
-//	logger.debug("executing \(inst)", metadata: ["skip": "\(skip)", "pc": "\(context.state.pc)"])
-//
-//	return inst.execute(context: context, skip: skip)
 func step(program Program, ctx Context) error {
 	pc := ctx.State.pc
 
@@ -55,4 +50,14 @@ func step(program Program, ctx Context) error {
 		// @TODO RETURN ERR
 		return nil
 	}
+
+	ctx.State.gas -= program.GasCostAt(pc)
+	ri := program.RegisterIndexesAt(pc)
+
+	switch i {
+	case OpAdd:
+		return add(ctx, ri, program.SkipAt(pc))
+	}
+
+	return nil
 }
